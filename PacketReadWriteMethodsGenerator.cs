e9b9f58f2032442729f94c2e5213c8b9f347ef66
Valk <sebastianbelle074@protonmail.com>
2026-02-01 20:12:03 -0600

    Add support for List<int>

--- PacketReadWriteMethodsGenerator.cs
+++ PacketReadWriteMethodsGenerator.cs
@@ -41,27 +41,66 @@ internal class PacketReadWriteMethodsGenerator
 
         var writeLines = new List<string>();
         var readLines = new List<string>();
+        var namespaces = new HashSet<string>();
 
         foreach (IPropertySymbol property in properties)
         {
-            writeLines.Add($"writer.Write({property.Name});");
+            string typeStr = property.Type.ToDisplayString();
+            string propName = property.Name;
+
+            if (typeStr == "System.Collections.Generic.List<int>")
+            {
+                writeLines.Add($"writer.Write({propName}.Count);");
+                writeLines.Add($"");
+                writeLines.Add($"for (int i = 0; i < {propName}.Count; i++)");
+                writeLines.Add($"{{");
+                writeLines.Add($"    writer.Write({propName}[i]);");
+                writeLines.Add($"}}");
+            }
+            else
+            {
+                writeLines.Add($"writer.Write({propName});");
+            }
         }
 
         foreach (IPropertySymbol property in properties)
         {
             string? suffix = ReadMethodSuffix.Get(property);
+            string propName = property.Name;
 
             if (suffix == null)
             {
-                context.Warn(property, $"Type {property.Type} not supported. Manually implement the write and read operations in the Write and Read overrides to get rid of this warning.");
-                return null;
-            }
+                string typeStr = property.Type.ToDisplayString();
 
-            readLines.Add($"{property.Name} = reader.Read{suffix}();");
+                if (typeStr == "System.Collections.Generic.List<int>")
+                {
+                    readLines.Add($"{propName} = new List<int>();");
+                    readLines.Add($"int {propName.ToLower()}Count = reader.ReadInt();");
+                    readLines.Add($"");
+                    readLines.Add($"for (int i = 0; i < {propName.ToLower()}Count; i++)");
+                    readLines.Add($"{{");
+                    readLines.Add($"    {propName}.Add(reader.ReadInt());");
+                    readLines.Add($"}}");
+
+                    namespaces.Add("System.Collections.Generic");
+                }
+                else
+                {
+                    context.Warn(property, $"Type {property.Type} not supported. Manually implement the write and read operations in the Write and Read overrides to get rid of this warning.");
+                    return null;
+                }
+            }
+            else
+            {
+                readLines.Add($"{property.Name} = reader.Read{suffix}();");
+            }  
         }
 
+        var usings = string.Join("\n", namespaces.Select(ns => $"using {ns};"));
         var indent8 = "        ";
         var sourceCode = $$"""
+{{usings}}
+
 namespace {{namespaceName}};
 
 public partial class {{className}}
